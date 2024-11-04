c28fad44832d2b6cfdc006f5ee526d85415dc137
Wasim6205 <wasimbhai6205@gmail.com>
2024-11-04 14:06:14 +0530

    ready to deploy

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,26 @@
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">WASIM</div>
+          <div className="right">
+            <p>Gomoh Dhanbad Jharkhand</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By MDWASIM</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Wasim.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
